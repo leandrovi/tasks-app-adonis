a5a2ac20fcdc69832be6bfdf9e0639aa25e380a0
leandrovi <leandrofernandesvieira@gmail.com>
2020-02-26 13:28:51 -0300

    feat: validating routes

--- start/routes.js
+++ start/routes.js
@@ -4,16 +4,27 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store').validator('User')
-Route.post('sessions', 'SessionController.store')
+Route.post('sessions', 'SessionController.store').validator('Session')
 
 Route.post('passwords', 'ForgotPasswordController.store')
+  .validator('ForgotPassword')
 Route.put('passwords', 'ForgotPasswordController.update')
+  .validator('ResetPassword')
 
 Route.get('/files/:file', 'FileController.show')
 
 Route.group(() => {
   Route.post('/files', 'FileController.store')
 
-  Route.resource('projects', 'ProjectController').apiOnly()
-  Route.resource('projects.tasks', 'TaskController').apiOnly()
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map([
+      [['project.store'], ['Project']]
+    ]))
+
+  Route.resource('projects.tasks', 'TaskController')
+    .apiOnly()
+    .validator(new Map([
+      [['project.tasks.store'], ['Task']]
+    ]))
 }).middleware(['auth'])
