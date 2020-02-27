7a6d3dc80c98e7df695e05d63c03d66a0a18edce
leandrovi <leandrofernandesvieira@gmail.com>
2020-02-27 07:48:03 -0300

    chore: dealing with cors

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,8 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: false,
+  origin: true,
+  // origin: [leandrovi.com.br],
 
   /*
   |--------------------------------------------------------------------------
